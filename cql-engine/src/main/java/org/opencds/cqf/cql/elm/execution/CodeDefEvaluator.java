dbf8dce020b49aed5fce466fa81bd6398e44925f
cnanjo@gmail.com <cnanjo@gmail.com>
2019-07-14 13:47:01 -0700

    Added withDisplay to CodeDefEvaluator. Unable to add withVersion because method not exposed by CodeDef. Why is this needed? Equal comparison on Code fail because display and version are not copied when present from the code definition.

--- cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/CodeDefEvaluator.java
+++ cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/CodeDefEvaluator.java
@@ -12,6 +12,6 @@ public class CodeDefEvaluator extends org.cqframework.cql.elm.execution.CodeDef 
     @Override
     public Object evaluate(Context context) {
         CodeSystemInfo info = (CodeSystemInfo) getCodeSystem().evaluate(context);
-        return new Code().withCode(this.getId()).withSystem(info.getId());
+        return new Code().withCode(this.getId()).withSystem(info.getId()).withDisplay(this.getDisplay());
     }
 }
